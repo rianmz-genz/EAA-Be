96aca74866c4806c0b16cba5fcc6e455c34638f4
rianmz-genz <adrianji1945@gmail.com>
2023-11-29 00:21:40 +0700

    last

--- manajemen_toko_api/controllers/sale.py
+++ manajemen_toko_api/controllers/sale.py
@@ -1,4 +1,5 @@
-from odoo import http, _
+from odoo import http, _, exceptions
+
 from odoo.http import request
 from odoo.addons.web.controllers.main import serialize_exception
 import json
@@ -9,8 +10,7 @@ class SaleApiController(http.Controller):
         self.helper = request.env['service.helper']
         self.sale_service = request.env['service.sale']
         
-    @http.route('/api/sale/<int:sale_id>', auth='public', methods=['GET'], csrf=False)
-    @serialize_exception
+    @http.route('/api/sale/<int:sale_id>', auth='public', methods=['POST'], csrf=False, cors="*")
     def get_sale(self, sale_id, **kw):
         try:
             sale_data = self.sale_service.get_by_id(sale_id)
@@ -18,17 +18,35 @@ class SaleApiController(http.Controller):
             return self.helper.res_json({}, False, f'Err {e}')
         return self.helper.res_json(sale_data, True, 'Berhasil mendapatkan penjualan')
     
-    @http.route('/api/sale/get_all', auth='public', methods=['GET'], csrf=False)
+    @http.route('/api/sale/get_all', auth='public', methods=['POST'], csrf=False, cors="*")
     def get_all_sales(self, **kw):
-        sale_data = self.sale_service.get_all()
+        kolom_dibutuhkan = ['uid']
+        try:
+            self.helper.validasi_kolom(kw, kolom_dibutuhkan)
+        except exceptions.ValidationError as e:
+            return self.helper.res_json([], False, f'Err {e}')
+        try:
+            sale_data = self.sale_service.get_all(kw)
+        except Exception as e:
+            return self.helper.res_json({}, False, f'Err {e}')
         return self.helper.res_json(sale_data, True, 'Berhasil mendapatkan semua penjualan')
 
-    @http.route('/api/sale/create', auth='public', methods=['POST'], csrf=False)
-    @serialize_exception
+    @http.route('/api/sale/create', auth='public', methods=['POST'], csrf=False, cors="*")
     def create_sale(self, **kw):
-        sale_data = json.loads(request.httprequest.data)
+        kolom_dibutuhkan = ['uid', 'data']
+        try:
+            self.helper.validasi_kolom(kw, kolom_dibutuhkan)
+        except exceptions.ValidationError as e:
+            return self.helper.res_json([], False, f'Err {e}')
+        uid = kw.get('uid')
+        if uid:
+            uid = int(kw.get('uid'))
+        else:
+            uid = 1
+        sale_data = json.loads(kw['data'])
         sale = request.env['new.sale'].sudo().create({
             'date': sale_data.get('date'),
+            'user_id': uid,
         })
         if 'line_ids' in sale_data:
             for line in sale_data['line_ids']:
