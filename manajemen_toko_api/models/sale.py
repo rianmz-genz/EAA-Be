f00da5f50895654c2cf0664efd1f55e0b626626e
rianmz-genz <adrianji1945@gmail.com>
2023-11-29 00:21:54 +0700

    last

--- manajemen_toko_api/models/sale.py
+++ manajemen_toko_api/models/sale.py
@@ -4,6 +4,7 @@ class NewSale(models.Model):
     _name = 'new.sale'
     _description = 'Manajemen Toko API'
 
+    user_id = fields.Many2one('res.users', string="User")
     line_ids = fields.One2many('new.saleline', 'sale_id')
     date = fields.Char()
     total = fields.Integer(compute="_compute_total")
